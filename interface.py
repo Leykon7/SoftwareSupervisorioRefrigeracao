80f275684214e0ff2460bf5fdce6bc6f0b6ec283
Mateus <86882032+Leykon7@users.noreply.github.com>
2023-01-16 02:08:53 -0300

    Atualização do domingo
    Adição de popups e um pouco do backend

--- interface.py
+++ interface.py
@@ -2,16 +2,102 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.animation import Animation
 from kivy.properties import NumericProperty
-from popups import comandoVent, comandoComp
+import popups
+#from popups import comandoVent,medidasVent, comandoComp, ModbusPopup, ScanPopup, medidasComp, inversor, info
+from pyModbusTCP.client import ModbusClient
+from kivy.core.window import Window
+from threading import Thread
+from time import sleep
+import datetime
 
 class Interface(BoxLayout):
     """
     Widget Principal da Applicação
     """
+    _updateThread = None
+    _updateWidgets = True
+    _tags = {}
+
     def __init__(self, **kwargs):
         super().__init__()
-        self._comandoVent = comandoVent()
-        self._comandoComp = comandoComp()
+        self._scan_time = kwargs.get('scan_time')
+        self._serverIP = kwargs.get('server_ip')
+        self._port = kwargs.get('server_port')
+        self._ModbusPopup = popups.ModbusPopup(self._serverIP,self._port)
+        self._ScanPopup = popups.ScanPopup(self._scan_time)
+
+        self._ClienteModbus = ModbusClient(host=self._serverIP, port=self._port)
+        # self._medidas = {}
+        # self._medidas['Timestamp']=None
+        # self._medidas['Valores']=None
+        # for key,value in kwargs.get('modbus_enderecos').items():
+        #     if key == 'fornalha':
+        #         cor_plot = (1,0,0,1)
+        #     else:
+        #         cor_plot =(0,1,0,1)
+        #     self._tags[key] = {'endereco': value, 'color': cor_plot}
+
+        self._comandoVent = popups.comandoVent()
+        self._medidasVent = popups.medidasVent()
+        self._medidasComp = popups.medidasComp()
+        self._comandoComp = popups.comandoComp()
+        #self._inversor = popups.inversor()
+    
+    def iniciaColetaDados(self,ip,port):
+        """
+        Método para configurar ip e porta e inicializar a thread de leitura dos dados
+        """
+        self._serverIP = ip
+        self._port = port
+        self._ClienteModbus.host = self._serverIP
+        self._ClienteModbus.port = self._port
+        try:
+            Window.set_system_cursor('wait')
+            self._ClienteModbus.open()
+            Window.set_system_cursor('arrow')
+            if self._ClienteModbus.is_open():
+                self._updateThread = Thread(target=self.atualizador)
+                self._updateThread.start()
+                self.ids.conexa.text = 'CONECTADO'
+                self._ModbusPopup.dismiss()
+            else:
+                self._ModbusPopup.porInfo('FALHA NA CONEXÃO!')
+        except Exception as e:
+            print("Erro", e.args)
+            self._ModbusPopup.porInfo('FALHA NA CONEXÃO!')
+
+    def atualizador(self):
+        """
+        Metodo que invoca leitura dos dados, atualização de interface e inserção no banco de dados
+        """
+        try:
+            while self._updateWidgets:
+                self.lerDados()
+                self.atualizaInterface()
+                #Inserir no banco de dados
+                sleep(self._scan_time/1000)
+        except Exception as e:
+            self._ClienteModbus.close()
+            print('Erro', e.args)
+
+
+
+    def lerDados(self):
+        """
+        Metodo de leitura de dados pelo protocolo modbus
+        """
+        # self._medidas['Timestamp'] = datetime.now()
+        # for key,value in self._tags.items():
+        #     self._medidas['valores'][key] = self._ClienteModbus.read_holding_register(value['endereco'],1)[0]
+
+    def atualizaInterface(self):
+        """
+        Método que atualiza a interface gráfica a partir dos dados lidos
+        """
+        # for key,value in self._tags.items():
+        #         self.ids[key].text = str(self.medidas['values'][key])+'ºC'
+
+
 
     def venezianas(self, *args):
         self.ids.veneziana1.angle = args[1]
@@ -29,6 +115,8 @@ class Interface(BoxLayout):
             self.ids.helice0.source ='imgs/helicebaixadaBrancasvg.png'
             self.ids.hel1.start()
             self.ids.hel0.stop()
+    def teste(self):
+        print(self.ids.tamanho.size)
 
 class Helices(FloatLayout):
     """
--- popups.py
+++ popups.py
@@ -1,9 +1,52 @@
 from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.utils import escape_markup
+
+class ModbusPopup(Popup):
+    """
+    popup para conectar no servidor
+    """
+    _infoLabel = None
+    def __init__(self,ip,porta,**kwargs):
+        super().__init__(**kwargs)
+        self._info = info()
+        self.ids.txt_ip.text=str(ip)
+        self.ids.txt_porta.text=str(porta)
+
+    def porInfo(self, mensagem):
+        self._infoLabel = Label(text='[b][color=ff3333]'+escape_markup(mensagem)+'[/color][/b]', markup=True)
+        self._info.setInfoLabel(self._infoLabel)
+        self._info.ids.info.add_widget(self._infoLabel)
+        self._info.open()
+
+class info(Popup):
+    """
+    Popup de informação sobre a conexão
+    """
+    _infoLabel = None
+    def setInfoLabel(self, info):
+        self._infoLabel = info
+
+    def limparInfo(self):
+        if self._infoLabel is not None:
+            self.ids.info.remove_widget(self._infoLabel)
+
+class ScanPopup(Popup):
+    """
+    popup para conectar no servidor
+    """
+    def __init__(self,scantime,**kwargs):
+        super().__init__(**kwargs)
+        self.ids.txt_st.text=str(scantime)
+    pass
 
 class comandoVent(Popup):
     """
     Comandos dos Ventiladores
     """
+    def __init__(self, **kwargs):
+        super().__init__()
+        self._inversor = inversor()
     pass
 
 class comandoComp(Popup):
@@ -16,9 +59,17 @@ class medidasVent(Popup):
     """
     Medidas dos Ventiladores
     """
+    def teste(self):
+        self.ids.tensaoMVent.text = '0 V'
 
 class medidasComp(Popup):
     """
     Medidas dos Ventiladores
+    """
+    pass
+
+class inversor(Popup):
+    """
+    Configurações do Inversor
     """
     pass
